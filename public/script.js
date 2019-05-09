bd5c8bffb0c1cb55e5ee84d653d4e82a77a652c5
Patrick <pa.lieberherr@outlook.com>
2019-05-09 21:17:59 +0200

    Improve booking form.

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-// alert("Gugus");
+function toggleActive(menuNo) {
+	var menu1ClassList = document.getElementById("menu1").classList;
+	var menu2ClassList = document.getElementById("menu2").classList;
+	var menu3ClassList = document.getElementById("menu3").classList;
+
+	if (menuNo === 1) menu1ClassList.add("active");
+	else menu1ClassList.remove("active");
+	if (menuNo === 2) menu2ClassList.add("active");
+	else menu2ClassList.remove("active");
+	if (menuNo === 3) menu3ClassList.add("active");
+	else menu3ClassList.remove("active");
+}
