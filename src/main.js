0405a937adf9a67ab05cec0d8247b07a6ca91c4b
Patrick <pa.lieberherr@outlook.com>
2019-05-19 16:08:26 +0200

    Fix Bootstrap.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "bootstrap"; // Bootstrap JS, Vue imports bootstrap's depedencies autmatically (jQuery and Popper)
+import "bootstrap/dist/css/bootstrap.css"; // Bootstrap CSS has to be imported manually
 
 Vue.config.productionTip = false;
 
