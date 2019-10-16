8faa4659ea6e5ed54c1a42ddee3112fd9232cbfd
Patrick Lieberherr <plieberherr@adaptricity.com>
2019-10-17 00:17:15 +0200

    Add new cook.

--- src/components/Cooks.vue
+++ src/components/Cooks.vue
@@ -1,13 +1,15 @@
 <template>
-<div class="invert">
-	<span v-for="cook in activeCooks">
-		<a @mouseover="selectedCook = cook">
-		<img v-bind:src="require('../../img/cooks/'+cook.id+'.jpg')" width="20%" >
-		</a>
-	</span>
-	<div v-if="selectedCook" class="pt-5 pb-3 cook-description">
-	<h1>{{selectedCook.nameOnPlatform}}</h1>
-	<p>{{selectedCook.descrGerman}}</p>
+<div>
+	<div class="invert">
+		<span v-for="cook in activeCooks">
+			<a @mouseover="selectedCook = cook">
+			<img v-bind:src="require('../../img/cooks/'+cook.id+'.jpg')" width="20%" >
+			</a>
+		</span>
+		<div v-if="selectedCook" class="pt-5 pb-3 cook-description">
+		<h1>{{selectedCook.nameOnPlatform}}</h1>
+		<p>{{selectedCook.descrGerman}}</p>
+		</div>
 	</div>
 </div>
 </template>
@@ -35,9 +37,10 @@ var data = function(){
 			new Cook("alauchiger",true,"Angela", "Lauchiger", "Angi", "Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.", "41791234567", "a@b.c", "Zurich"),
 			new Cook("aspumoni",false,"Angelo","Spumoni","Angelo","Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et.","41791234567","a@b.c","Zurich"),
 			new Cook("amastali",true,"Ahmed","Mastali","Ahmed","Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et.","41791234567","a@b.c","Zurich"),
-			new Cook("ldimasi",true,"Laura","Dimasi","Laura","Tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.","41791234567","a@b.c","Zurich"),
+			new Cook("imentzer",true,"Iris","Mentzer","Iris","Dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.","41791234567","a@b.c","Zurich"),
+			new Cook("ldimasi",false,"Laura","Dimasi","Laura","Tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.","41791234567","a@b.c","Zurich"),
 			new Cook("rschuppisser",false,"Rafael","Schupisser","Raffi","Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.","41791234567","a@b.c","Zurich"),
-			new Cook("rkuenz",true,"Reto","Kuenz","Reto","Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.","41791234567","a@b.c","Zurich")
+			new Cook("rkuenz",true,"Reto","Kuenz","Reto","Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.","41791234567","a@b.c","Zurich"),
 			],
 		selectedCook: undefined
 	}
@@ -57,7 +60,7 @@ export default {
 
 <style scoped>
 img{
-	  /* object-fit: cover; */
+	/* object-fit: cover; */
 	-webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
   	filter: grayscale(100%);
 	  	transition:1s;
@@ -72,11 +75,6 @@ img:hover{
 
 a {
 	cursor: pointer
-}
-
-.invert{
-  background-color:#000000cb;
-  color:white;
 }
 
 .cook-description p{
