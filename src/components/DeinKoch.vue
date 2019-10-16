85f93e4657fb0eea158ff2e2a7cd04b6e9ffa297
Patrick Lieberherr <plieberherr@adaptricity.com>
2019-10-17 00:16:25 +0200

    Dein Koch... pictograms instead of images.

--- src/components/DeinKoch.vue
+++ src/components/DeinKoch.vue
@@ -1,20 +1,20 @@
 <template>
 	<div class="row mt-5">
         <figure class="text-center col-6 col-md-3">
-          <img src="../../img/static/deinkoch2.jpg" class="img-fluid dein-koch-img" alt="Kauft ein">
-          <figcaption class="h3 mt-2">Kauft ein</figcaption>
+          <img src="../../img/static/deinkoch2-icon.jpg" class="img-fluid dein-koch-img" alt="Kauft ein">
+          <figcaption class="mt-2">Kauft ein</figcaption>
         </figure>
         <figure class="text-center col-6 col-md-3">
-          <img src="../../img/static/deinkoch3.jpg" class="img-fluid dein-koch-img" alt="Serviert">
-          <figcaption class="h3 mt-2">Deckt den Tisch</figcaption>
+          <img src="../../img/static/deinkoch4-icon.jpg" class="img-fluid dein-koch-img" alt="Serviert">
+          <figcaption class="mt-2">Kocht</figcaption>
         </figure>
         <figure class="text-center col-6 col-md-3">
-          <img src="../../img/static/deinkoch4.jpg" class="img-fluid dein-koch-img" alt="Kocht">
-          <figcaption class="h3 mt-2">Kocht</figcaption>
+          <img src="../../img/static/deinkoch5-icon.jpg" class="img-fluid dein-koch-img" alt="Kocht">
+          <figcaption class="mt-2">Serviert</figcaption>
         </figure>
         <figure class="text-center col-6 col-md-3">
-          <img src="../../img/static/deinkoch6.jpg" class="img-fluid dein-koch-img" alt="Räumt auf">
-          <figcaption class="h3 mt-2">Räumt auf</figcaption>
+          <img src="../../img/static/deinkoch7-icon.jpg" class="img-fluid dein-koch-img" alt="Räumt auf">
+          <figcaption class="mt-2">Räumt auf</figcaption>
         </figure>
     </div>
 </template>
@@ -23,11 +23,8 @@
 
 <style scoped>
 img {
-	  -webkit-filter: grayscale(20%); /* Safari 6.0 - 9.0 */
-	  filter: grayscale(100%) brightness(80%);
-	    border-radius: 30%;
 		float:middle;
-		width:200px;
+		width:100px;
 		/* clip-path: polygon(85% 0, 100% 50%, 85% 100%, 0% 100%, 15% 50%, 0% 0%); */
 }
 </style>
